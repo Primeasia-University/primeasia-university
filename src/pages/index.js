b4d0744d767704f964492d06a37f79c127720d7f
soykot2910 <md.soykot2910@gmail.com>
2020-12-15 19:29:44 +0600

    noticeBoard added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,15 @@
 import React from "react"
 
-import Layout from "../layout/layout";
-import Hero from "../components/Home/Hero";
+import Layout from "../layout/layout"
+import Hero from "../components/Home/Hero"
 
 const IndexPage = () => (
   <Layout>
-    <Hero/>
+    <Hero />
   </Layout>
 )
 
 export default IndexPage
-
 
 // font-family: 'Bree Serif', serif;
 // font-family: 'Lato', sans-serif;
